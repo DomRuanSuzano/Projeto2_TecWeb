be0d58bf58385ae22a26de701c13d6f631b33846
DomRuanSuzano <dom.ruan.drs@gmail.com>
2022-10-09 21:51:47 -0300

    CRUD implementado e docker funcionando

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,5 @@
+from ast import Not
+from turtle import title
 from django.shortcuts import render, redirect
 from .models import Note
 
@@ -6,8 +8,30 @@ def index(request):
     if request.method == 'POST':
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
-        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        nota = Note(title=title, content=content)
+        nota.save()
         return redirect('index')
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
+
+def delete(request):
+    id = request.POST.get('id')
+    nota = Note.objects.get(id=id)
+    nota.delete()
+    return redirect('index')
+
+def index_update(request):
+    id = request.POST.get('id')
+    note = Note.objects.get(id=id)
+    return render(request, 'notes/update.html', {'note': note})
+
+def update(request):
+    title = request.POST.get('titulo')
+    content = request.POST.get('detalhes')
+    id = request.POST.get('id')
+    nota = Note.objects.get(id=id)
+    nota.title = title
+    nota.content = content
+    nota.save()
+    return redirect('index')
